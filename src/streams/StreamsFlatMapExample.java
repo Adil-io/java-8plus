51da14299fcf1d9c6e8b52bbcc8c1b3c17208e4e
adil_yusuf <adil_yusuf@epam.com>
2025-03-16 13:59:32 +0530

    Streams distinct, count, sorted

--- src/streams/StreamsFlatMapExample.java
+++ src/streams/StreamsFlatMapExample.java
@@ -12,10 +12,21 @@ public class StreamsFlatMapExample {
         return StudentDataBase.getAllStudents().stream() // Stream<Student>
                 .map(Student::getActivities) // Stream<List>
                 .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> -> distinct function performed
+                .sorted() // Stream<String> -> sorts input in the natural order(a - z)
                 .collect(Collectors.toList()); // List<String>
+    }
+
+    public static Long getStudentActivitiesCount() {
+        return StudentDataBase.getAllStudents().stream() // Stream<Student>
+                .map(Student::getActivities) // Stream<List>
+                .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> -> distinct function performed
+                .count(); // Long
     }
 
     public static void main(String[] args) {
         System.out.println("Print Student activities: " + getStudentActivities());
+        System.out.println("getStudentActivitiesCount: " + getStudentActivitiesCount());
     }
 }
