2515d19f7c36577d95dd771cfbd86bbd4ac50c29
adil_yusuf <adil_yusuf@epam.com>
2025-03-21 15:41:54 +0530

    Streams Reduce Min

--- src/streams/StreamsMinMaxExample.java
+++ src/streams/StreamsMinMaxExample.java
@@ -10,6 +10,7 @@ public class StreamsMinMaxExample {
     private static int findMaxValue(List<Integer> integerList) {
         return integerList.stream()
                 // int passed in stream in order one-by-one 6, 7, 8, 9, 10
+                // x is assigned the initial value (in this case) -> 0
                 // y -> 6, 7, 8, 8, 10
                 // x -> holds the max value for each element in the iteration
                 .reduce(0, (x, y) -> x > y ? x : y);
@@ -19,6 +20,21 @@ public class StreamsMinMaxExample {
         return integerList.stream()
                 // .reduce((x, y) -> x > y ? x : y);
                 .reduce(Integer::max);
+    }
+
+    private static int findMinValue(List<Integer> integerList) {
+        return integerList.stream()
+                // int passed in stream in order one-by-one 6, 7, 8, 9, 10
+                // x is assigned the initial value (in this case) -> 0
+                // y -> 6, 7, 8, 8, 10
+                // x -> holds the min value for each element in the iteration
+                .reduce(0, (x, y) -> x < y ? x : y);
+    }
+
+    private static Optional<Integer> findMinValueOptional(List<Integer> integerList) {
+        return integerList.stream()
+                // .reduce((x, y) -> x < y ? x : y);
+                .reduce(Integer::min);
     }
 
     public static void main(String[] args) {
@@ -37,6 +53,24 @@ public class StreamsMinMaxExample {
         maxValueOptional = findMaxValueOptional(emptyList);
         if (maxValueOptional.isPresent()) {
             System.out.println("findMaxValueOptional: " + maxValueOptional.get());
+        } else {
+            System.out.println("Input List is empty.");
+        }
+
+        // Invalid as 0 is used as an initial value
+        // Note: Be mindful while using initial value in reduce function
+        System.out.println("findMinValue: " + findMinValue(integerList));
+
+        Optional<Integer> minValueOptional = findMinValueOptional(integerList);
+        if (minValueOptional.isPresent()) {
+            System.out.println("findMaxValueOptional: " + minValueOptional.get());
+        } else {
+            System.out.println("Input List is empty.");
+        }
+
+        minValueOptional = findMinValueOptional(emptyList);
+        if (minValueOptional.isPresent()) {
+            System.out.println("findMaxValueOptional: " + minValueOptional.get());
         } else {
             System.out.println("Input List is empty.");
         }
